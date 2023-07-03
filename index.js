7a9a365390348474097e66fc3d9ee8e48216ccb4
MichaelCai0301 <030michaelcai030@gmail.com>
2023-07-03 10:34:33 -0400

    cstuff

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ console.log(process.env.NODE_ENV);
 app.use(cors());
 app.use(express.json());
 // process.env.NODE_ENV returns "production" or undefined
+
 if (process.env.NODE_ENV === "production") {
     //if it's in production mode
     //server static content
